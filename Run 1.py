fe7d90b6e51d2d82051aa652a3abf7982219095a
Albert-Hanstein <hgunnoo@outlook.com>
2018-11-29 19:11:38 +0000

    Set up means for navigating directories.

--- Run 1.py
+++ Run 1.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 from sklearn import preprocessing
@@ -7,5 +8,22 @@ resized_img = cv2.resize(img, (16, 16))
 resized_img = np.array(resized_img, dtype=float)
 flattened = resized_img.ravel()
 flattened2 = np.vstack([flattened, flattened])
+# Only do this once all the images in the folder are saved
 normalised = preprocessing.normalize(flattened2, norm='l1')
-print(sum(normalised[1]))
+
+folder = [x[0] for x in os.walk('../training/')]
+folder = folder[1:]
+#print(folder)
+for path in folder:
+    print(path)
+    os.chdir(path)
+    """
+    Save images here
+    """
+    os.chdir('../')
+    """
+    test_image = cv2.imread('0.jpg', cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('img', test_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
