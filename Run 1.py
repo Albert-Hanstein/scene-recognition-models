36ede10cd358c9a68f587c385342ef293d078f16
Albert-Hanstein <hgunnoo@outlook.com>
2018-11-29 16:15:30 +0000

    Resize and flatten an image into a 1D Numpy array

--- Run 1.py
+++ Run 1.py
@@ -1,7 +1,13 @@
 import cv2
-import numpy
+import numpy as np
 
 img = cv2.imread('../training/bedroom/0.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image',img)
+resized_img = cv2.resize(img, (16, 16))
+resized_img = np.array(resized_img, dtype=float)
+flattened = resized_img.ravel()
+print(flattened)
+"""
+cv2.imshow('image',resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+"""
