580029d162cfd530a6cf0043e1890251f1a91d29
Albert-Hanstein <hgunnoo@outlook.com>
2018-11-29 18:38:23 +0000

    Make a normalised numpy array from flattened 16x16 image

--- Run 1.py
+++ Run 1.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
+from sklearn import preprocessing
 
-img = cv2.imread('../training/bedroom/0.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('../training/bedroom/1.jpg', cv2.IMREAD_GRAYSCALE)
 resized_img = cv2.resize(img, (16, 16))
 resized_img = np.array(resized_img, dtype=float)
 flattened = resized_img.ravel()
-print(flattened)
-"""
-cv2.imshow('image',resized_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
+flattened2 = np.vstack([flattened, flattened])
+normalised = preprocessing.normalize(flattened2, norm='l1')
+print(sum(normalised[1]))
