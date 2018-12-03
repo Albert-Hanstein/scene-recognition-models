53aa714fc28b6bba6d39436e7f0d8783e08834d5
Albert-Hanstein <hgunnoo@outlook.com>
2018-12-03 20:05:12 +0000

    Parse through image, get no. of samples. Next:save samples.

--- Run 2/run_2.py
+++ Run 2/run_2.py
@@ -3,3 +3,7 @@ from sampling import sample
 
 path = '../training/bedroom/'
 sample(path)
+
+
+# TODO: Research sampling with OpenCV
+# There should be a way 
--- Run 2/sampling.py
+++ Run 2/sampling.py
@@ -4,7 +4,21 @@ import cv2
 def sample(path):
     os.chdir(path)
     test_image = cv2.imread('0.jpg', cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('img', test_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    height, width = test_image.shape
+    x_start_sample = 0
+    y_start_sample = 0
+    num_of_samples = 0
+    while y_start_sample <= (height - 8):
+        while x_start_sample <= (width - 8):
+            num_of_samples += 1
+            x_start_sample += 4
+        x_start_sample = 0
+        y_start_sample += 4
+    print("Number of samples: " + str(num_of_samples)) # 3185
+    # crop_img = test_image[0:100, 0:100]
+    # cv2.imshow('crop1', crop_img)
+    # cv2.imshow('orig', test_image)
+    # cv2.imshow('crop2', test_image[150:200, 0:100])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return;
